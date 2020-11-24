8efb994450b25a23732214f1a07ec419c747e0b5
Calvintoes <cxt6655@rit.edu>
2020-11-24 18:03:33 -0500

    fix build errors

--- public/assets/icons/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as LOADING } from './Interwind.gif';
-export { default as SEARCH } from './search.svg';
-export { default as RIGHT_ARROW } from './chevron-right.svg';
--- utils/utils.tsx
+++ utils/utils.tsx
@@ -1,10 +1,15 @@
 import { useState, useEffect } from 'react';
 
+interface WindowSize {
+  width: number | undefined;
+  height: number | undefined;
+}
+
 // Hook
 export function useWindowSize() {
   // Initialize state with undefined width/height so server and client renders match
   // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-  const [windowSize, setWindowSize] = useState({
+  const [windowSize, setWindowSize] = useState<WindowSize>({
     width: undefined,
     height: undefined,
   });
